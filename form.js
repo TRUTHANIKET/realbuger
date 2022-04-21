114a3b2e76889fa86feaa75363a668656c1d4bd1
ANIKET AAGE NAHI PATA <100013837+TRUTHANIKET@users.noreply.github.com>
2022-04-21 19:10:07 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,17 @@
+alert("YOUR ORDER IS BEING PREPARED ");
+alert("FILL THE FORM FOR THE DELIVERY PURPOSE")
+
+
+function myfunc(){
+    let a=document.getElementById('name').value;
+     let c=document.getElementById('area').value; 
+      let b=document.getElementById('pass').value;
+       const d=new Date();
+    document.write('<span style="font-family: Quicksand, sans-serif">',"<center>","<b>FIRST NAME IS :      ",a,"<br>",
+    "SURNAME IS :     ",b,"<br>" ,"ADDRESS IS:",c, "<br><br><br><br><br><br><br><br><br><br>","THE DELIVERY WILL PROCEED IN A SHORT DURATION","<br><br><br><br><br><br><br><br><br><br></b>",d,'</span>');
+
+
+  
+  
+  ;
+}
